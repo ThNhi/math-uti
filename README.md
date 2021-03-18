aef4017523ee1e7fc7a154336df443d80edbbb34
ThNhi <phongthanhnhi1812@gmail.com>
2021-03-18 15:16:27 +0700

    Add README.md & getFactorial() method

---
+++ README.md
@@ -0,0 +1,2 @@
+## Welcome to Math-Util repository. This repo offer various math
+methods used in your projects as the helper ones.
---
+++ src/com/thnhi/MathUtility.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thnhi;
+
+/**
+ *
+ * @author DELL
+ */
+public class MathUtility {
+    //day la class mo phong lai class Math, chua cac ham sai chung
+    //ham sai chung, ko luu lai dieu gi -> static
+
+    public static final double PI = 3.1415;
+
+    //ham tinh n!
+    //n! tang rat nhanh ve gia tri
+    //21! tran` kieu long, hon 18 so 0
+    //bai nay ta tinh tu 0...20!. Am ko tinh dc, 0! = 1! = 1
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("n must be betwen 0...20");
+        }
+        //song sot den day thi n = 0...20
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        long result = 0; //gia bo = 0
+        for (int i = 2; i <= n; i++) {
+            result *= 1;
+        }
+        return result;
+    }
+}
