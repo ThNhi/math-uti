28e426f7f70755a785d0ed3c07a504417bdeef2b
ThNhi <phongthanhnhi1812@gmail.com>
2021-03-25 16:28:29 +0700

    RECURSION UPDATE

--- src/com/fu/mathutil/MathUtility.java
+++ src/com/fu/mathutil/MathUtility.java
@@ -23,15 +23,20 @@ public class MathUtility {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be betwen 0...20");
         }
+        
+        //sua code sua ham ! cho gon, dung de quy (recursion) : goi lai chinh minh voi 1 quy mo khac
+        //STACKOVERFLOW.COM
         //song sot den day thi n = 0...20
         if (n == 0 || n == 1) {
             return 1;
         }
-
-        long result = 1; //gia bo = 0
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        return  n * getFactorial(n - 1);
+        //n! = n * (n -1)!
+//
+//        long result = 1; //gia bo = 0
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
     }
 }
